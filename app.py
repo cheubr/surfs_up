e93d616653c409cd575f568d46609a9901e5d496
Brandon Cheung <cheung.brandoncheung@gmail.com>
2022-02-27 20:17:11 -0500

    upload deliverable 1 & 2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import datetime as dt
-from venv import create
 import numpy as np
 import pandas as pd
 import sqlalchemy
@@ -54,3 +53,21 @@ def temp_monthly():
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
 
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")   
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
